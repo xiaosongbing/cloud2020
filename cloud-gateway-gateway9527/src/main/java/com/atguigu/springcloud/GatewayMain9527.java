a9ed47cf88c35f9e98dfd1553501886f7f2d0cc0
wangzuzhen <380429867@qq.com>
2020-04-03 16:39:55 +0800

    cloud-gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GatewayMain9527.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @version 1.0
+ * @ClassName: GatewayMain9527
+ * @Description: TODO
+ * @Author jiguangpao
+ * @Date: 2020/4/3 10:33
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/TestPredicate.java
@@ -0,0 +1,18 @@
+import com.atguigu.springcloud.GatewayMain9527;
+import org.springframework.boot.SpringApplication;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @version 1.0
+ * @ClassName: TestPredicate
+ * @Description: TODO
+ * @Author jiguangpao
+ * @Date: 2020/4/3 11:57
+ */
+public class TestPredicate {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
