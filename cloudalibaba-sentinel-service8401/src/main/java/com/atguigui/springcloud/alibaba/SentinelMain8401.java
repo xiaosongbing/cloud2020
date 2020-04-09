0108f4740bc1c61d5989bf4c6fe4673015bdaa24
wangzuzhen <380429867@qq.com>
2020-04-09 17:48:01 +0800

    springcloud Alibaba Sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigui/springcloud/alibaba/SentinelMain8401.java
@@ -0,0 +1,20 @@
+package com.atguigui.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @version 1.0
+ * @ClassName: SentinelMain8401
+ * @Description: TODO
+ * @Author jiguangpao
+ * @Date: 2020/4/9 14:27
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelMain8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigui/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,47 @@
+package com.atguigui.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @version 1.0
+ * @ClassName: FlowLimitController
+ * @Description: TODO
+ * @Author jiguangpao
+ * @Date: 2020/4/9 14:30
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
+        return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+
+        log.info("testD 异常比例");
+        int age = 10 / 0;
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数");
+        int age = 10 / 0;
+        return "------testE 测试异常数";
+    }
+}
